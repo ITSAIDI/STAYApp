271b4dfa545c42372e717db2e0edce1d22601f65
ITSAIDI <noureddinesaidi111@gmail.com>
2025-07-15 01:34:38 +0200

    c

--- app/networks/page.jsx
+++ app/networks/page.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 export default function networks() {
   return (
     <div >
-       <h1>Networks</h1>
+       <h1>Networks hello I'm here man of Code !</h1>
     </div>
   )
 }
